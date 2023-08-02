124f5bfee51b86d494d0ca7afe4ec8417783ea57
nekorina7 <70853934+nekorina7@users.noreply.github.com>
2023-08-02 20:22:17 +0700

    v1.0

---
+++ Bloody Roar 2 Launcher/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace FileDownloaderApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.downloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proxyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.downloadToolStripMenuItem,
+            this.proxyToolStripMenuItem,
+            this.playToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(338, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // downloadToolStripMenuItem
+            // 
+            this.downloadToolStripMenuItem.Name = "downloadToolStripMenuItem";
+            this.downloadToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.downloadToolStripMenuItem.Text = "Install";
+            this.downloadToolStripMenuItem.Click += new System.EventHandler(this.DownloadToolStripMenuItem_Click);
+            // 
+            // proxyToolStripMenuItem
+            // 
+            this.proxyToolStripMenuItem.Name = "proxyToolStripMenuItem";
+            this.proxyToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.proxyToolStripMenuItem.Text = "Proxy";
+            this.proxyToolStripMenuItem.Click += new System.EventHandler(this.proxyToolStripMenuItem_Click);
+            // 
+            // playToolStripMenuItem
+            // 
+            this.playToolStripMenuItem.Name = "playToolStripMenuItem";
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.playToolStripMenuItem.Text = "Play";
+            this.playToolStripMenuItem.Click += new System.EventHandler(this.playToolStripMenuItem_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.AccessibleName = "";
+            this.progressBar1.Location = new System.Drawing.Point(12, 286);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(314, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(338, 321);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Bloody Roar 2 Launcher";
+            this.Click += new System.EventHandler(this.MainForm_Click);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void MainForm_Click(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem downloadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proxyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ Bloody Roar 2 Launcher/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileDownloaderApp
+{
+    public partial class MainForm : Form
+    {
+        private bool isPaused = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void DownloadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fileUrl = "https://dl.dropboxusercontent.com/s/psp1ehj322ntxpb/BR2%20Online%20by%20NK.zip?dl=0";
+                string savePath = "BR2 Online by NK.zip";
+
+                if (!File.Exists(savePath))
+                {
+                    using (WebClient webClient = new WebClient())
+                    {
+                        isPaused = false;
+                        await DownloadFileAsync(webClient, new Uri(fileUrl), savePath);
+                        ExtractZipFile(savePath);
+                        MessageBox.Show("The installation process is complete.", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    string extractPath = Path.GetDirectoryName(savePath);
+                    string zipFileName = Path.GetFileNameWithoutExtension(savePath);
+                    string destinationPath = Path.Combine(extractPath, zipFileName);
+
+                    if (!Directory.Exists(destinationPath))
+                    {
+                        ExtractZipFile(savePath);
+                        MessageBox.Show("The installation process is complete.", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("The game folder already exists.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred during the download: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task DownloadFileAsync(WebClient webClient, Uri fileUrl, string savePath)
+        {
+            var taskCompletionSource = new TaskCompletionSource<object>();
+
+            void DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+            {
+                webClient.DownloadFileCompleted -= DownloadFileCompleted;
+                taskCompletionSource.SetResult(null);
+            }
+
+            void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+            {
+                progressBar1.Value = e.ProgressPercentage;
+            }
+
+            webClient.DownloadFileCompleted += DownloadFileCompleted;
+            webClient.DownloadProgressChanged += DownloadProgressChanged;
+            await webClient.DownloadFileTaskAsync(fileUrl, savePath);
+
+            await taskCompletionSource.Task;
+        }
+
+        private void ExtractZipFile(string zipFilePath)
+        {
+            string extractPath = Path.GetDirectoryName(zipFilePath);
+            string zipFileName = Path.GetFileNameWithoutExtension(zipFilePath);
+            string destinationPath = Path.Combine(extractPath, zipFileName);
+
+            if (!Directory.Exists(destinationPath))
+            {
+                ZipFile.ExtractToDirectory(zipFilePath, destinationPath);
+            }
+        }
+        private void proxyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string gameFolder = "BR2 Online by NK"; // Change this to the actual folder name if it's different
+            string proxyExePath = Path.Combine(gameFolder, "proxy.exe");
+
+            if (File.Exists(proxyExePath))
+            {
+                try
+                {
+                    Process.Start(proxyExePath);
+                }
+                catch (Exception ex)
+                {
+                    // Show an error message if there was an issue starting the process
+                    MessageBox.Show($"Failed to run proxy.exe: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("proxy.exe not found in the game folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void playToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string gameFolder = "BR2 Online by NK"; // Change this to the actual folder name if it's different
+            string duckstationExePath = Path.Combine(gameFolder, "duckstation-qt-x64-ReleaseLTCG.exe");
+            string romFilePath = Path.Combine(gameFolder, "roms/SCUS-94424.cue");
+            string stateFilePath = Path.Combine(gameFolder, "savestates/netplay/SCUS-94424.sav");
+            string arguments = $"-fastboot \"{romFilePath}\" -statefile \"{stateFilePath}\"";
+
+            if (File.Exists(duckstationExePath))
+            {
+                try
+                {
+                    Process.Start(duckstationExePath, arguments);
+                }
+                catch (Exception ex)
+                {
+                    // Show an error message if there was an issue starting the process
+                    MessageBox.Show($"Failed to start duckstation-qt-x64-ReleaseLTCG.exe: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("duckstation-qt-x64-ReleaseLTCG.exe not found in the game folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
